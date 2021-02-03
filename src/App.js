fcf403245f9f74de1a9a7dd6a7ed3cb64ad6f66f
ramprits <ramprits-sdei@gmail.com>
2021-02-03 22:17:12 +0530

    Adding Login and Register page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import IndexPage from './pages/Index.js';
+import IndexPage from "./pages/Index.js";
+import LoginPage from "./pages/login.js";
+import RegisterPage from "./pages/register.js";
 
 export default function App() {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/">
-            <IndexPage />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route exact path="/">
+          <IndexPage />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/register">
+          <RegisterPage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
